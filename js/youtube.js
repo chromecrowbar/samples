2aabea6cb31578a65922e57a6a26450da11675d9
chromecrowbar <luis@chromecrowbar.com>
2014-04-01 21:11:24 -0400

    youtube jquery plugin

---
+++ js/youtube.js
@@ -0,0 +1,20 @@
+/**
+ * Simple YouTube Plugin
+ * @param {string} id YouTube id
+ * @param {object} options {width,height}
+ * @returns {$.fn} jQuery object
+ */
+$.fn.youtube = function (id,options) {
+    var test = /[0-9a-zA-Z]{11}/;
+    
+    //youtube id is an 11 character alphanumeric
+    if (test.test(id)) {
+        var opts = {
+            width : 560,
+            height : 315
+        };
+        $.extend(opts,options);
+        this.append('<iframe width="'+ opts.width +'" height="'+ opts.height +'" src="//www.youtube.com/embed/'+ id + '" frameborder="0" allowfullscreen></iframe>');
+    }
+    return this;    
+};
